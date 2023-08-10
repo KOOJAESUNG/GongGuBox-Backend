1cad27e48db3ba8eab585970fc6c0441b9e7e236
gujaeseong <gjs5448@gmail.com>
2023-08-11 02:12:24 +0900

    Swagger /docs 로 접근 가능하도록 설정

--- src/main/java/com/gonggubox/GongGuBoxApplication.java
+++ src/main/java/com/gonggubox/GongGuBoxApplication.java
@@ -2,12 +2,34 @@ package com.gonggubox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
+@EnableJpaAuditing //Auditing 사용
 public class GongGuBoxApplication {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GongGuBoxApplication.class, args);
 	}
 
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return route(GET("/docs"), req ->
+				ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build());
+	}
 }
