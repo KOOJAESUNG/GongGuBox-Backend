b2607a14a7d5d5baf7d44872246317577c5e9bb2
gujaeseong <gjs5448@gmail.com>
2023-08-11 01:22:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/gonggubox/GongGuBoxApplication.java
@@ -0,0 +1,13 @@
+package com.gonggubox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongGuBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongGuBoxApplication.class, args);
+	}
+
+}
