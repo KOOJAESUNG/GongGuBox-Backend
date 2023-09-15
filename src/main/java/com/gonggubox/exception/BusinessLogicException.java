3acdb7492560baf01ab2511022036fea136f6589
gujaeseong <gjs5448@gmail.com>
2023-09-15 16:02:11 +0900

    Exception Controller handling 로직 추가

---
+++ src/main/java/com/gonggubox/exception/BusinessLogicException.java
@@ -0,0 +1,13 @@
+package com.gonggubox.exception;
+
+public class BusinessLogicException extends RuntimeException{
+
+    private ExceptionCode exceptionCode;
+
+    public BusinessLogicException(ExceptionCode exceptionCode){
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+
+    public ExceptionCode getExceptionCode() { return exceptionCode; }
+}
---
+++ src/main/java/com/gonggubox/exception/GlobalExceptionAdvice.java
@@ -0,0 +1,100 @@
+package com.gonggubox.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionAdvice {// 프로젝트 전역에서 발생하는 모든 예외 잡아 handler로 처리한다
+
+
+    /**
+     * Validation error
+     *
+     * @Valid 검증 실패시 작동
+     */
+    @ExceptionHandler //  발생한 특정 예외를 잡아서 하나의 메소드에서 공통 처리해줄 수 있게 해준다.
+    @ResponseStatus(HttpStatus.BAD_REQUEST)/* 400 BAD_REQUEST : 잘못된 요청 */
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getBindingResult());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)/* 400 BAD_REQUEST : 잘못된 요청 */
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException e) {
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getConstraintViolations());
+
+        return response;
+    }
+
+
+    /**
+     * 잘못된 RequestMethod로 Request
+     */
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ErrorResponse handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED, e.getMessage());
+        return response;
+    }
+
+
+    /**
+     * null 혹은 적절하지 않은 parameter를 넘겨주었을 경우
+     * ex) exRepository.findById(null);
+     */
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException e) {
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.FORBIDDEN, e.getMessage());
+
+        return response;
+    }
+
+
+    /**
+     * @RequestParam으로 받아온 값이 null인 경우
+     */
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
+        return response;
+    }
+
+    /**
+     * @RequestBody로 받아온 값의 value가 null인 경우 (key만 있는 경우)
+     */
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
+
+        return response;
+    }
+
+
+    /**
+     * Custom Exception Handler
+     */
+    @ExceptionHandler
+    public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getExceptionCode());
+
+        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getExceptionCode().getHttpStatus().value())
+        );
+    }
+
+}
