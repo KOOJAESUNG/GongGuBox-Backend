d6cc1bc54bf6fca7f46331efc8380cc90c5153b9
gujaeseong <gjs5448@gmail.com>
2023-10-07 07:16:55 +0900

    swagger 라우팅규칙 application.yml로 이동

--- src/main/java/com/gonggubox/GongGuBoxApplication.java
+++ src/main/java/com/gonggubox/GongGuBoxApplication.java
@@ -2,16 +2,7 @@ package com.gonggubox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
-
-import java.net.URI;
-
-import static org.springframework.web.servlet.function.RequestPredicates.GET;
-import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 @EnableJpaAuditing //Auditing 사용
@@ -27,9 +18,12 @@ public class GongGuBoxApplication {
 		SpringApplication.run(GongGuBoxApplication.class, args);
 	}
 
-	@Bean
-	RouterFunction<ServerResponse> routerFunction() {
-		return route(GET("/docs"), req ->
-				ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build());
-	}
+	/**
+	 * application.yml 설정으로 대체하였음!!
+	 */
+//	@Bean
+//	RouterFunction<ServerResponse> routerFunction() {
+//		return route(GET("/docs"), req ->
+//				ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build());
+//	}
 }
